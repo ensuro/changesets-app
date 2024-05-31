9cd595bd1490e22ac117af381ddb476636ecd572
GnP <g.parrondo@gmail.com>
2024-05-31 18:46:01 -0300

    Remove a lot of unnecessary dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -6,28 +6,32 @@ module.exports = {
 
     // https://github.com/ChainSafe/web3.js#web3-and-create-react-app
     Object.assign(fallback, {
-      crypto: require.resolve("crypto-browserify"),
-      stream: require.resolve("stream-browserify"),
-      assert: require.resolve("assert"),
-      http: require.resolve("stream-http"),
-      https: require.resolve("https-browserify"),
-      os: require.resolve("os-browserify"),
-      url: require.resolve("url"),
+      // crypto: require.resolve("crypto-browserify"),
+      // stream: require.resolve("stream-browserify"),
+      // assert: require.resolve("assert"),
+      // http: require.resolve("stream-http"),
+      // https: require.resolve("https-browserify"),
+      // os: require.resolve("os-browserify"),
+      // url: require.resolve("url"),
       // https://stackoverflow.com/questions/68707553/uncaught-referenceerror-buffer-is-not-defined
-      buffer: require.resolve("buffer"),
+      buffer: require.resolve("buffer/"),
+      process: false,
+      assert: false,
+      stream: false,
+      "web-vitals": false,
     });
 
     config.resolve.fallback = fallback;
 
-    config.plugins = (config.plugins || []).concat([
-      new webpack.ProvidePlugin({
-        process: "process/browser",
-        Buffer: ["buffer", "Buffer"],
-      }),
-    ]);
+    // config.plugins = (config.plugins || []).concat([
+    //   new webpack.ProvidePlugin({
+    //     process: "process/browser",
+    //     Buffer: ["buffer", "Buffer"],
+    //   }),
+    // ]);
 
     // https://github.com/facebook/create-react-app/issues/11924
-    config.ignoreWarnings = [/to parse source map/i];
+    config.ignoreWarnings = [/node_modules/i];
 
     return config;
   },
