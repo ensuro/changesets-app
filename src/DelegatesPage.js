7a65a617ff80290b295e554a59bbfa27ae3566ef
GnP <g.parrondo@gmail.com>
2024-06-04 03:03:53 -0300

    Refactor for page per drawer item

---
+++ src/DelegatesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DelegatesPage() {
+  return <div>Delegates Page</div>;
+}
+
+export default DelegatesPage;
---
+++ src/TransactionsPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+
+import { useSafe } from "./safe-ui";
+
+import SafeSummary from "./SafeSummary";
+import Transactions from "./Transactions";
+
+function TransactionsPage() {
+  const safe = useSafe();
+
+  return (
+    <Grid container spacing={3}>
+      {/* Recent Deposits */}
+      <Grid item xs={12}>
+        <Paper
+          sx={{
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <SafeSummary address={safe.safeAddress} />
+        </Paper>
+      </Grid>
+      {/* Transactions */}
+      <Grid item xs={12}>
+        <Paper sx={{ p: 2, display: "flex", flexDirection: "column" }}>
+          <Transactions />
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default TransactionsPage;
