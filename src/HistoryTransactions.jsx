4272b692a82deaaa03e5dafc2237c7c6efd74b73
LucasCambon <lucambon08@gmail.com>
2025-11-10 16:59:46 -0300

    Missing Divider import

--- src/HistoryTransactions.jsx
+++ src/HistoryTransactions.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Stack, Button, Typography } from "@mui/material";
+import { Stack, Button, Typography, Divider } from "@mui/material";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 import TransactionCard from "./TransactionCard";
